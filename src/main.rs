3683a45f7673d18a49275fb99255682d47717fc3
flotji-potji <nme199911@gmail.com>
2022-06-05 01:16:43 +0200

    tried out to build a chapter picker, did not work out

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::io;
+
 mod rust_beginning;
 
 fn main() {
-    rust_beginning::say_hello();
+    println!("This is my journy through the Rust programming language.");
+    println!("Each chapter is organized in its own program");
+    println!("Please select a chapter which should be executed:");
+    println!("\t Chapter 1: Hello World!");
+    
+    let mut pick = String::new();
+
+    io::stdin()
+        .read_line(&mut pick)
+        .expect("Error in handling input!");
+
+    if pick.eq("1") {
+        rust_beginning::say_hello();
+    }
 }
