70fb52718aa02a2a0804195f42fcddd9aa696e89
flotji-potji <nme199911@gmail.com>
2022-07-05 23:59:07 +0200

    finished with chapter 2 guessing game

--- src/chapter2_guessing_game.rs
+++ src/chapter2_guessing_game.rs
@@ -18,7 +18,11 @@ pub fn guessing_game() {
             .read_line(&mut guess)
             .expect("Failed to rad line!");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
 
         println!("You guessed: {guess}");
 
