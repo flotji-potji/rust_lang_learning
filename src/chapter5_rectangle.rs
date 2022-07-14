1065c15e66b655b8441467465ef75e7b659fef4a
flotji-potji <nme199911@gmail.com>
2022-07-14 22:21:15 +0200

    now a beautifully finished version using a struct element

--- src/chapter5_rectangle.rs
+++ src/chapter5_rectangle.rs
@@ -1,14 +1,22 @@
 // Chapter 5 - Using Structs to Structure Related Data: An Example Program Using Structs
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 pub fn start_rectangles() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        area(&rect1)
     );
 }
 
-fn area(dimensions: (u32,u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
