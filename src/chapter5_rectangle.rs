7f25b956ab4f0a2745f7472136238c4b55eadae3
flotji-potji <nme199911@gmail.com>
2022-07-14 23:06:07 +0200

    added an associated method to init Rectangle more easily if we have Squares!

--- src/chapter5_rectangle.rs
+++ src/chapter5_rectangle.rs
@@ -11,12 +11,20 @@ impl Rectangle {
         self.width * self.height
     }
 
-    fn width(self: &Self) -> bool {     // "self: &Self" same as "&self"
+    fn width(self: &Self) -> bool {
+        // "self: &Self" same as "&self"
         self.width > 0
     }
 
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
     }
 }
 
@@ -61,13 +69,15 @@ pub fn start_rectangles() {
     };
 
     let rect3 = Rectangle {
-        width: 60, 
+        width: 60,
         height: 45,
     };
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect2 hold rect3? {}", rect2.can_hold(&rect3));
 
+    let sq1 = Rectangle::square(30);
+    println!("Look at my awesome Square it has a perfect area of: {}", sq1.area());
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
