9d41751ba756ba87591dc6ddb22249ce54ed5a75
flotji-potji <nme199911@gmail.com>
2022-07-14 22:17:20 +0200

    a version of area with a tuple attribute that's not so obvious to read

--- src/chapter5_rectangle.rs
+++ src/chapter5_rectangle.rs
@@ -1,15 +1,14 @@
 // Chapter 5 - Using Structs to Structure Related Data: An Example Program Using Structs
 
 pub fn start_rectangles() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = (30, 50);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(rect1)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(dimensions: (u32,u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
