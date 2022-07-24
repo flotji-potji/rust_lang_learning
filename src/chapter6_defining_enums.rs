cc1fc68c1e36be72a5849b49890d4e2d7a5a20f2
flotji-potji <nme199911@gmail.com>
2022-07-24 23:34:56 +0200

    the correct way to store enum kinds in enums!

--- src/chapter6_defining_enums.rs
+++ src/chapter6_defining_enums.rs
@@ -1,25 +1,13 @@
 // Chapter 6 - Enums and Pattern Matching: Defining an Enum
 
 enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+    V4(String),
+    V6(String),
 }
 
 pub fn enum_fun() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    let loopback = IpAddrKind::V6(String::from("::1"));
 }
 
-fn route(ip_kind: IpAddrKind) {}
